3e19e80410d742b41799fe826b84d6f4ed00219f
Chaitali Barhate <chaitali.barhate8472@gmail.com>
2025-07-11 00:02:10 +0530

    Pagination changes

--- src/app/features/recipes/components/recipies-list/recipes-list.component.ts
+++ src/app/features/recipes/components/recipies-list/recipes-list.component.ts
@@ -9,6 +9,8 @@ import { RecipesService } from '../../services/recipes.service';
 })
 export class RecipesListComponent implements OnInit {
   public recipesList: any[] = [];
+  public totalCount: number = 0;
+  public recordsPerPage: number = 10;
   constructor(private recipesService: RecipesService, private route: ActivatedRoute) {
 
   }
@@ -16,8 +18,11 @@ export class RecipesListComponent implements OnInit {
   getRecipesList(page:Number) {
     this.recipesService.getRecipiesList(page).subscribe({
       next: (data) => {
-        this.recipesList = data;
+        this.recipesList = data[0].recipeData;
+        this.totalCount = data[0].total[0]?.totalCount;
         console.log('Data >>> ', this.recipesList);
+        console.log('totalCount >>> ', this.totalCount);
+
       }
     }) 
   }
--- src/app/shared/components/pagination/pagination.component.ts
+++ src/app/shared/components/pagination/pagination.component.ts
@@ -1,14 +1,42 @@
-import { Component, OnInit, Input } from '@angular/core';
-import { Router } from '@angular/router';
+import { Component, OnInit, OnChanges, Input, SimpleChanges } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-pagination',
   templateUrl: './pagination.component.html',
   styleUrls: ['./pagination.component.scss']
 })
-export class PaginationComponent implements OnInit {
+export class PaginationComponent implements OnInit, OnChanges {
+  public currentPage = 1;
+  public pagination = ['<< prev', 'next >>'];
+
+  constructor(private route: ActivatedRoute) { }
+
+  @Input() totalRecords:number = 0;
+  @Input() size:number = 10;
+
+  ngOnChanges(changes: SimpleChanges): void {
+
+  }
+
+  createPagination(current:number) {
+    let pageCount = Math.ceil(this.totalRecords/this.size);
+    console.log('current >> ', current);
+    if(current === 1) {      
+      this.pagination.splice(1,0, current.toString());
+      this.pagination.splice(2,0, pageCount.toString());
+    } else {
+      this.pagination.splice(1,0, (current-1).toString());
+      this.pagination.splice(2,0, current.toString());
+      this.pagination.splice(3,0, pageCount.toString());
+    }    
+  }
   
   ngOnInit(): void {
-    
+    this.route.queryParamMap.subscribe(params => {
+      this.currentPage = Number(params.get('p'));
+      console.log('Pagination >>> ', this.currentPage);
+      this.createPagination(this.currentPage);
+    })
   }
 }
